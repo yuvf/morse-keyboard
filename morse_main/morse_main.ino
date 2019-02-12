004af968d5b468015ea08e046a219d6718957dcb
Yuval Fisher <fisheryuval@gmail.com>
2019-02-12 02:21:55 +0200

    Identify if the morse key is up or down

--- morse_main/morse_main.ino
+++ morse_main/morse_main.ino
@@ -1,45 +1,91 @@
-const int buttonPin = 11;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+#include <Bounce2.h>
+
+const int UNIT = 300;
+
+enum separatorType
+{
+  space=UNIT*7,
+  dot=UNIT*1,
+  line=UNIT*3,
+  part_letter=UNIT*1,
+  letter=UNIT*3,
+  error=0,
+};
+
+const int button_pin = 11;     // the number of the pushbutton pin
+const int led_pin =  13;      // the number of the LED pin
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-int isDown = 0;
+Bounce morse_key = Bounce(button_pin, 10);
+separatorType separator = space;
+int button_state = 0;         // variable for reading the pushbutton status
+unsigned long last_time = 0;
+unsigned long gap_time = 0;
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(led_pin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(button_pin, INPUT);
   Serial.begin(9600);
-  Serial.write("Test2");
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH)
-  {
-    Serial.print("HIGH");
-  }
-  else
-  {
-    Serial.print("LOW");
-  }
+  Serial.println("Starting...");
+  last_time = millis();
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    if (isDown == 0)
+  if (morse_key.update())
+  {
+    if (morse_key.risingEdge())
     {
-      //Serial.write("Button down");
-      isDown = 1;
+      key_up();
     }
-    
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-  } else {
-    // turn LED off:
-    isDown = 0;
-    digitalWrite(ledPin, LOW);
+    else if (morse_key.fallingEdge())
+    {
+      key_down();
+    }
   }
+}
+
+void key_down()
+{
+  Serial.print("Down time: ");
+  Serial.print(millis() - last_time);
+  Serial.println();
+  last_time = millis();
+  
+  // turn LED on:
+  digitalWrite(led_pin, LOW);
+
+  separator = check_separation();
+  Serial.println(separator);
+}
+
+separatorType check_separation()
+{
+  gap_time = millis() - last_time;
+  Serial.println(space);
+  if (gap_time >= space)
+  {
+    return space;
+  }
+  if (gap_time >= letter)
+  {
+    return letter;
+  }
+  if (gap_time >= part_letter)
+  {
+    return part_letter;
+  }
+  return error;
+}
+
+void key_up()
+{
+  Serial.print("Up time: ");
+  Serial.print(millis() - last_time);
+  Serial.println();
+  last_time = millis();
+
+  // turn LED off:
+  digitalWrite(led_pin, HIGH);
 }
